c6fd9f9b1f065f8633876ca0053dbeefc42e07d9
MartinCrane <martin.crane@gmail.com>
2017-03-01 17:39:45 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250 }
+  validates :category, inclusion: {in: %w(Fiction Non-fiction) }
+  validate  :clickbait
+
+  def clickbait
+    
+    if title.present? && title.scan(Regexp.union([/Won\'t Believe/, /Secret/, /Top\d/, /Guess/])).empty?
+      errors.add(:title, "Title error")
+    end
+  end
+
 end
